40d82be381227d88a309f00c76ee3495db094f3c
Mohammad Aslam <Taiyab.aslam728@gmail.com>
2023-01-12 15:08:14 -0600

    second one works

--- test/tests.js
+++ test/tests.js
@@ -1,5 +1,5 @@
 // IMPORT MODULES under test here:
-import { myFunction, addExclamationPoints, multiplyBySeven } from '../functions.js';
+import { myFunction, addExclamationPoints, multiplyBySeven, multiplyBy12ThenHalve, divideThenMultiply } from '../functions.js';
 
 const { test, skip } = QUnit;
 
@@ -19,10 +19,26 @@ test('this test should add exclamation points', (expect) => {
     expect.equal(actual, expected, 'bunny rabbit = bunny rabbit!!!');
 });
 
-test('This function should take in a number and multiply it by twelve then cut the result in half', (expect) => {
+test('This function should take in a number and multiply it by seven', (expect) => {
     const expected = 28;
 
     const actual = multiplyBySeven(4);
 
     expect.deepEqual(actual, expected, '4 * 7 should be 28');
+});
+
+test('This function should take in a number and multiply it by twelve then cut the result in half', (expect) => {
+    const expected = 24;
+
+    const actual = multiplyBy12ThenHalve(4);
+
+    expect.deepEqual(actual, expected, '(4 * 12)/2');
+});
+
+test('This function should take in three numbers, divide the first by the second, then multiply the result by the third', (expect) => {
+    const expected = 10;
+
+    const actual = divideThenMultiply(8, 4, 5);
+
+    expect.deepEqual(actual, expected, '(8/4)5' );
 });
