8dcb5a9de2e1e551e523d73a825f3faf055289b4
Mohammad Aslam <Taiyab.aslam728@gmail.com>
2023-01-12 20:11:28 -0600

    I AM DONE!!!

--- test/tests.js
+++ test/tests.js
@@ -181,12 +181,44 @@ test('This function should take in two numbers and return a greeting announcing 
     expect.deepEqual(actual, expected, '8, 4 should be Hello! Your lucky number for the day is 12');
 });
 
+test('This function should take in two numbers and return a greeting announcing that the sum of those numbers is todays lucky number', (expect2) => {
+    const expected2 = 'Hello! Your lucky number for the day is 3.';
+
+    const actual2 = makeLuckyGreeting(1, 2);
+
+    expect2.deepEqual(actual2, expected2, '1, 2 should be Hello! Your lucky number for the day is 3');
+});
+
+test('This function should take in two numbers and return a greeting announcing that the sum of those numbers is todays lucky number', (expect3) => {
+    const expected3 = 'Hello! Your lucky number for the day is 9.';
+
+    const actual3 = makeLuckyGreeting(4, 5);
+
+    expect3.deepEqual(actual3, expected3, '4, 5 should be Hello! Your lucky number for the day is 9');
+});
+
 test('This function should take an array and return the second item in the array', (expect) => {
     const expected = 'apple';
 
     const actual = getSecondItem(['kiwi', 'apple', 'orange', 'plum']);
 
     expect.deepEqual(actual, expected, 'should be apple');
+});
+
+test('This function should take an array and return the second item in the array', (expect2) => {
+    const expected2 = 'bruhzelsprouts';
+
+    const actual2 = getSecondItem(['kiwi', 'bruhzelsprouts', 'orange', 'plum']);
+
+    expect2.deepEqual(actual2, expected2, 'should be bruhzelsprouts');
+});
+
+test('This function should take an array and return the second item in the array', (expect3) => {
+    const expected3 = 'dogfood';
+
+    const actual3 = getSecondItem(['kiwi', 'dogfood', 'orange', 'plum']);
+
+    expect3.deepEqual(actual3, expected3, 'should be dogfood');
 });
 
 test('This function should take an array and return the LAST item in the array, no matter the arrays length', (expect) => {
@@ -197,6 +229,23 @@ test('This function should take an array and return the LAST item in the array, 
     expect.deepEqual(actual, expected, 'should be plum');
 });
 
+test('This function should take an array and return the LAST item in the array, no matter the arrays length', (expect2) => {
+    const expected2 = 'I need food';
+
+    const actual2 = getLastItem(['kiwi', 'apple', 'orange', 'I need food']);
+
+    expect2.deepEqual(actual2, expected2, 'should be I need food');
+});
+
+test('This function should take an array and return the LAST item in the array, no matter the arrays length', (expect3) => {
+    const expected3 = 'im so tired';
+
+    const actual3 = getLastItem(['kiwi', 'apple', 'orange', 'im so tired']);
+
+    expect3.deepEqual(actual3, expected3, 'should be im so tired');
+});
+
+
 test('This function should take a dog object and return an <li> with the name of the dog', (expect) => {
     const expected = '<li>Benny</li>';
 
@@ -205,11 +254,43 @@ test('This function should take a dog object and return an <li> with the name of
     expect.deepEqual(actual.outerHTML, expected, 'should be name of dog');
 });
 
+test('This function should take a dog object and return an <li> with the name of the dog', (expect2) => {
+    const expected2 = '<li>I like cats</li>';
+
+    const actual2 = renderDogLI({ name: 'I like cats', age: 6 });
+
+    expect2.deepEqual(actual2.outerHTML, expected2, 'should be name of dog');
+});
+
+test('This function should take a dog object and return an <li> with the name of the dog', (expect3) => {
+    const expected3 = '<li>almost done</li>';
+
+    const actual3 = renderDogLI({ name: 'almost done', age: 6 });
+
+    expect3.deepEqual(actual3.outerHTML, expected3, 'should be name of dog');
+});
+
 test('This function should take a dog object and return a div with the dogs information', (expect) => {
     const expected = '<div><h1>Benny</h1><p>Benny is 6 years old</p></div>';
 
     const actual = renderDogDiv({ name: 'Benny', age: 6 });
 
     expect.deepEqual(actual.outerHTML, expected, 'should be information of dog');
+});
+
+test('This function should take a dog object and return a div with the dogs information', (expect2) => {
+    const expected2 = '<div><h1>DAISY</h1><p>DAISY is 900 years old</p></div>';
+
+    const actual2 = renderDogDiv({ name: 'DAISY', age: 900 });
+
+    expect2.deepEqual(actual2.outerHTML, expected2, 'should be information of dog');
+});
+
+test('This function should take a dog object and return a div with the dogs information', (expect3) => {
+    const expected3 = '<div><h1>I am gonna have such a good nap after this</h1><p>I am gonna have such a good nap after this is 1 years old</p></div>';
+
+    const actual3 = renderDogDiv({ name: 'I am gonna have such a good nap after this', age: 1 });
+
+    expect3.deepEqual(actual3.outerHTML, expected3, 'should be information of dog');
 });
 
